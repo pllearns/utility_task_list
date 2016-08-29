fcc6b20a12df52e161e6f91ada001e16dff6c76c
Harman Singh <harmanlearns@gmail.com>
2016-08-29 16:54:56 -0700

    wip: added authentication, cleaned up code, fixed npm start & build

--- src/app.js
+++ src/app.js
@@ -3,23 +3,19 @@ var path = require('path')
 var favicon = require('serve-favicon')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
+var cookieSession = require('cookie-session')
 var bodyParser = require('body-parser')
-
-var routes = require('./routes/index')
-var users = require('./routes/users')
+var database = require('./database')
+var routes = require('./routes')
 
 var app = express()
 
+app.get('env') === process.env.NODE_ENV || 'development'
 // view engine setup
-app.use(express.static(path.join(__dirname, '/public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 app.set('port', (process.env.PORT || 3000))
-
-app.listen(app.get('port'), () => {
-  console.log('Example app listening on port 3000!')
-})
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
@@ -28,14 +24,14 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(cookieSession( {
+app.use(cookieSession({
   name: 'session',
   keys: [
     'd96a5bad00f0f5bbfa7fa03ba42bab02a229ebd69',
     '09b75924af0e502cd1fad019ac6efdade87971019'
   ]
 }))
-app.use((req, res) => {
+app.use((req, res, next) => {
   req.loggedIn = !!req.session.userId
   req.getCurrentUser = getCurrentUser
   next()
@@ -50,7 +46,6 @@ const getCurrentUser = () => {
 }
 
 app.use('/', routes);
-app.use('/users', users)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -83,5 +78,6 @@ app.use(function(err, req, res, next) {
   })
 })
 
-
-module.exports = app;
+app.listen(app.get('port'), () => {
+  console.log('Example app listening on port 3000!')
+})
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,27 +1,30 @@
 var express = require('express')
 var router = express.Router()
+var users = require('./users')
 // var db = require('./database')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  if(!req.loggedIn){
-  res.render('index', { title: 'Express' });
+
+  res.render('index', {
+    title: 'Tasker'
+  })
   return;
 })
 
-router.get('/login', = (req,res) => {
+router.get('/login', (req,res) => {
   res.render('login', {
     error: 'Please Log In'
   })
 })
 
-router.get('/signup', = (req,res) => {
+router.get('/signup', (req,res) => {
   res.render('signup',{
     email: ''
   })
 })
 
-router.post('/login', = (req,res) => {
+router.post('/login', (req,res) => {
   const email = req.body.email
   const password = req.body.password
   db.authenticateUser(email, password)
@@ -42,7 +45,36 @@ router.post('/login', = (req,res) => {
     })
 })
 
+router.post('/signup', (req,res) => {
+  const attributes = req.body.user
+  const email = attributes.email
+  const password = attributes.password
+  const password_confirmation = attributes.password_confirmation
+    if(password !== '' && password !== password_confirmation){
+      res.render('signup', {
+        error: 'Passwords Do Not Match',
+        email: email,
+      })
+    } else{
+      db.createUser(attributes)
+        .then(user => {
+          req.session.userId = user.id
+          res.redirect('/')
+        })
+        .catch(error => {
+          res.render('index', {
+            error: error,
+            email: email,
+          })
+        })
+    }
+})
 
+router.get('/logout', (req,res) => {
+  req.session = null
+  res.redirect('/')
+})
 
+router.use('/users', users)
 
 module.exports = router
