f62c92abf94941faf7a2c34b9b6d25558e347f38
Phillip Lorenzo <pllearns@gmail.com>
2016-09-02 09:35:11 -0700

    Working on delete user function (#10)

--- src/app.js
+++ src/app.js
@@ -62,6 +62,7 @@ app.use('/users', routes)
 app.get('/signup', routes)
 app.post('/signup', routes)
 app.delete('/tasks/:id', routes)
+  // app.delete('/users/:id', routes)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
